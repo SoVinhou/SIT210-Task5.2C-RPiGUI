3ba3b75a86dedd39d6be6ed675dfe53ddb5cd51d
SoVinhou <101162260+SoVinhou@users.noreply.github.com>
2022-04-22 20:53:34 +1000

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -18,18 +18,30 @@ myFont = tkinter.font.Font(family = 'Arial', size = 16, weight = "bold")
 def RedledToggler():
     if Redled.is_lit:
         Redled.off()
-        RedLedToggleButton["text"] = "Turn Red LED On"
+        RedLedToggleButton["text"] = "Turn Red LED On"    
     else:
         Redled.on()
         RedLedToggleButton["text"] = "Turn Red LED Off"
+        
+        Blueled.off()
+        BlueLedToggleButton["text"] = "Turn Blue LED On"
+        
+        Greenled.off()
+        GreenLedToggleButton["text"] = "Turn Green LED On" 
 
 def BlueledToggler():
     if Blueled.is_lit:
         Blueled.off()
-        BlueLedToggleButton["text"] = "Turn Blue LED On"
+        BlueLedToggleButton["text"] = "Turn Blue LED On" 
     else:
         Blueled.on()
         BlueLedToggleButton["text"] = "Turn Blue LED Off"
+        
+        Redled.off()
+        RedLedToggleButton["text"] = "Turn Red LED On"
+        
+        Greenled.off()
+        GreenLedToggleButton["text"] = "Turn Green LED On"  
         
 def GreenledToggler():
     if Greenled.is_lit:
@@ -38,8 +50,16 @@ def GreenledToggler():
     else:
         Greenled.on()
         GreenLedToggleButton["text"] = "Turn Green LED Off"
+                
+        Redled.off()
+        RedLedToggleButton["text"] = "Turn Red LED On"
         
-
+        Blueled.off()
+        BlueLedToggleButton["text"] = "Turn Blue LED On"
+        
+def quitGUI():
+    RPi.GPIO.cleanup
+    win.destroy()
 
 ## CREATE WIDGETS
 RedLedToggleButton = Button(win, text = 'Turn Red LED On', font = myFont, command = RedledToggler, bg = 'Red', height = 1, width = 50) ## CREATE RED LED WIDGET
@@ -50,4 +70,7 @@ BlueLedToggleButton.grid(row=1,column=1)
 
 GreenLedToggleButton = Button(win, text = 'Turn Green LED On', font = myFont, command = GreenledToggler, bg = 'Green', height = 1, width = 50) ## CREATE BLUE LED WIDGET
 GreenLedToggleButton.grid(row=2,column=1)
+
+ExitButton = Button(win, text = 'Exit', font = myFont, command = quitGUI, bg = 'Gray', height = 1, width = 50) ## CREATE BLUE LED WIDGET
+ExitButton.grid(row=3,column=1)
 
